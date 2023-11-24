3525407839e67d7f7b27976383748ab2d4fa0cef
Temirlan <temirlan.asanov.ta@gmail.com>
2023-11-24 15:12:14 +0600

    as

--- database/migrations/2023_11_24_082756_add_group_id_to_employees_table.php
+++ database/migrations/2023_11_24_082756_add_group_id_to_employees_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('employees', function (Blueprint $table) {
             //
-            $table->bigInteger('group_id')->nullable();
+            // $table->bigInteger('group_id')->nullable();
         });
     }
 
