33e1ce9ee39b00c15fdae06c6a30cf7da91ba4bf
Temirlan <temirlan.asanov.ta@gmail.com>
2024-01-11 11:53:31 +0600

    group

---
+++ database/migrations/2024_01_11_055250_add_group_id_nullable_to_employees_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            //make group_id nullable
+            $table->unsignedBigInteger('group_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            //make group_id not nullable
+            $table->unsignedBigInteger('group_id')->change();
+        });
+    }
+};
