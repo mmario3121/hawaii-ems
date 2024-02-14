da6077123ce43febf26603f40a69da81a4c89a6a
Temirlan <temirlan.asanov.ta@gmail.com>
2024-02-14 19:05:11 +0600

    test

---
+++ database/migrations/2024_02_14_130221_add_salary_to_employees_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->integer('salary_net')->nullable()->change();
+            $table->integer('salary_gross')->nullable()->change();
+            $table->string('iin')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            //
+        });
+    }
+};
