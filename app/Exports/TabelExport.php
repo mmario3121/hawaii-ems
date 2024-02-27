e1560fed67f4fb2e92c3c80a80e434a28f67a1a0
Temirlan <temirlan.asanov.ta@gmail.com>
2024-02-27 17:31:46 +0600

    excel

--- app/Exports/TabelExport.php
+++ app/Exports/TabelExport.php
@@ -5,6 +5,7 @@ namespace App\Exports;
 use App\Models\Employee;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Events\AfterSheet;
 use Carbon\Carbon;
 
 class TabelExport implements FromCollection, WithHeadings
@@ -21,34 +22,29 @@ class TabelExport implements FromCollection, WithHeadings
     }
 
     public function collection()
-{
-    $startDate = Carbon::createFromDate($this->year, $this->month, 1, 'Asia/Almaty');
-    $endDate = $startDate->copy()->endOfMonth();
+    {
+        $startDate = Carbon::createFromDate($this->year, $this->month, 1, 'Asia/Almaty');
+        $endDate = $startDate->copy()->endOfMonth();
 
-    return Employee::where('department_id', $this->departmentId)
-        ->with(['workdays' => function ($query) use ($startDate, $endDate) {
-            $query->whereBetween('date', [$startDate, $endDate])
-                ->select('id', 'employee_id', 'date', 'workhours', 'isWorkday');
-        }])
-        ->get()
-        ->map(function ($employee) use ($startDate, $endDate) {
-            $row = [
-                'Employee ID' => $employee->id,
-                'Name' => $employee->name,
-            ];
-
-            foreach ($employee->workdays as $workday) {
-                $style = ['font' => ['color' => ($workday->isWorkday == 1) ? ['argb' => 'FF008000'] : ['argb' => 'FF808080']]];
-                $row[$workday->date] = [
-                    'value' => $workday->workhours,
-                    'style' => $style,
+        return Employee::where('department_id', $this->departmentId)
+            ->with(['workdays' => function ($query) use ($startDate, $endDate) {
+                $query->whereBetween('date', [$startDate, $endDate])
+                    ->select('id', 'employee_id', 'date', 'workhours', 'isWorkday');
+            }])
+            ->get()
+            ->map(function ($employee) use ($startDate, $endDate) {
+                $row = [
+                    'Employee ID' => $employee->id,
+                    'Name' => $employee->name,
                 ];
-            }
 
-            return $row;
-        });
-}
+                foreach ($employee->workdays as $workday) {
+                    $row[$workday->date] = $workday->workhours;
+                }
 
+                return $row;
+            });
+    }
 
     public function headings(): array
     {
@@ -68,29 +64,23 @@ class TabelExport implements FromCollection, WithHeadings
         return $headings;
     }
 
-    public function conditionalFormats(): array
+    public function registerEvents(): array
     {
         return [
-            'A2:ZZ1000' => [
-                [
-                    'rule' => '==1',
-                    'stopIfTrue' => true,
-                    'style' => [
-                        'font' => [
-                            'color' => ['argb' => 'FF008000'],
-                        ],
-                    ],
-                ],
-                [
-                    'rule' => '==0',
-                    'stopIfTrue' => true,
-                    'style' => [
-                        'font' => [
-                            'color' => ['argb' => 'FF808080'],
-                        ],
-                    ],
-                ],
-            ],
+            AfterSheet::class => function (AfterSheet $event) {
+                $sheet = $event->sheet;
+
+                // Define range and formatting rules
+                $range = 'A2:' . $sheet->getHighestColumn() . $sheet->getHighestRow();
+                $style = [
+                    'font' => ['color' => ['rgb' => '008000']],
+                    // Green color for workdays
+                ];
+
+                // Apply conditional formatting
+                $sheet->getStyle($range)->getFont()->getColor()->setARGB('FF808080');
+                // Grey color for non-workdays
+            },
         ];
     }
 }
