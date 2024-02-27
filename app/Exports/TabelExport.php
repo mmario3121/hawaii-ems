62791815f6af177ce8c9b74e3f930f5f51ceab41
Temirlan <temirlan.asanov.ta@gmail.com>
2024-02-27 18:28:51 +0600

    excel

--- app/Exports/TabelExport.php
+++ app/Exports/TabelExport.php
@@ -27,9 +27,11 @@ class TabelExport implements FromCollection, WithHeadings
         // Convert year and month to a date range
         $startDate = Carbon::createFromDate($this->year, $this->month, 1, 'Asia/Almaty');
         $endDate = $startDate->copy()->endOfMonth();
-        dd(Workday::where('employee_id', 69)->where('date', '>=', $startDate) // Include start date
-        ->where('date', '<=', $endDate)   // and end date
-        ->select('id', 'employee_id', 'date', 'workhours')->get());
+        dd($startDate);
+        // dd(Workday::where('employee_id', 69)->where('date', '>=', $startDate) // Include start date
+        // ->where('date', '<=', $endDate)   // and end date
+        // ->select('id', 'employee_id', 'date', 'workhours')->get());
+
         // Adjust the query as per your actual database structure and needs
         return Employee::where('department_id', $this->departmentId)
             ->with(['workdays' => function ($query) use ($startDate, $endDate) {
