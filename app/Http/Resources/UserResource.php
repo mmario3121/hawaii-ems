bd9ded0c253d4aa476df3de44bcb3332fe50d7a3
Temirlan <temirlan.asanov.ta@gmail.com>
2024-04-09 18:00:51 +0500

    changes

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -13,21 +13,14 @@ class UserResource extends JsonResource
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
-{
-    $roleTranslations = [
-        'manager' => 'Менеджер',
-        'admin' => 'Админ',
-        'treasurer' => 'Директор Ресторана',
-        'hr' => 'HR',
-    ];
-    $role = is_array($this->role()) ? $this->role()[0] : $this->role();
-    return [
-        'id' => $this->id,
-        'name' => $this->name,
-        'phone' => $this->phone,
-        'email' => $this->email,
-        'role' => [$roleTranslations[$role] ?? $role],
-        'image' => $this->image_url,
-    ];
-}
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'phone' => $this->phone,
+            'email' => $this->email,
+            'role' => $this->role(),
+            'image' => $this->image_url,
+        ];
+    }
 }
