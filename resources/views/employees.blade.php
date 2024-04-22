391e8b085d6936bb8cfe0911b81194290060917d
Temirlan <temirlan.asanov.ta@gmail.com>
2024-04-23 03:23:28 +0500

    excel

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -1,19 +1,29 @@
 <?php
 use App\Models\Holiday;
 ?>
+<style>
+    th, td {
+        width: 50px;
+        height: 50px;
+        text-align: center;
+    }
+    .wide {
+        width: 200px;
+    }
+</style>
 <table>
     <thead>
         <tr>
-            <th>Employee</th>
+            <th class="wide">ФИО</th>
             @foreach ($dates as $date)
-                <th>{{ $date->format('d.m.Y') }}</th>
+                <th>{{ $date->format('d') }}</th>
             @endforeach
-            <th>Workdays Last Month</th>
-            <th>Workhours</th>
-            <th>Norm</th>
-            <th>Workhours - Norm</th>
-            <th>Company BIN</th>
-            <th>Company Title</th>
+            <th>Дни</th>
+            <th>Часы</th>
+            <th>Норма</th>
+            <th>+/-</th>
+            <th class="wide">БИН</th>
+            <th class="wide">Компания</th>
         </tr>
     </thead>
     <tbody>
@@ -30,7 +40,7 @@ use App\Models\Holiday;
                     <td style="
                         {{ $workday && $workday->absence ? 'background-color: ' . $workday->absence->color . ';' : '' }}
                         {{ $isHoliday ? 'background-color: red;' : '' }}
-                        {{ $workday && $workday->workhours > 0 ? 'background-color: green;' : '' }}
+                        {{ $workday && $workday->isWorkday ? 'background-color: green;' : '' }}
                     ">
                         @if ($workday && $workday->absence)
                             {{ $workday->absence->type }}
@@ -44,7 +54,12 @@ use App\Models\Holiday;
                 <td>{{ $employee->workdays_last_month($year_month) ?? "0" }}</td>
                 <td>{{ $employee->workhours($year_month) ?? "0" }}</td>
                 <td>{{ $employee->norm($year_month) ?? "0" }}</td>
-                <td>{{ strval(($employee->workhours($year_month) ?? 0) - ($employee->norm($year_month) ?? 0)) }}</td>
+                @php
+                    $workhoursNormDiff = ($employee->workhours($year_month) ?? 0) - ($employee->norm($year_month) ?? 0);
+                @endphp
+                <td style="color: {{ $workhoursNormDiff >= 0 ? 'green' : 'red' }}">
+                    {{ strval($workhoursNormDiff) }}
+                </td>
                 <td>{{ " ".strval($employee->company->bin) }}</td>
                 <td>{{ $employee->company->title }}</td>
             </tr>
