ca4742ebdaf3bac687127151a05f25c2e0b9e599
Temirlan <temirlan.asanov.ta@gmail.com>
2024-05-07 00:46:26 +0500

    export

--- resources/views/prem.blade.php
+++ resources/views/prem.blade.php
@@ -58,25 +58,25 @@ use App\Models\Holiday;
                 <td width="5">{{ $employee->getShiftName() }}</td>
                 <td>{{ number_format($employee->salary_gross, 0, '', '') }}</td>
                 @php
-                    if($employee->workhours($request->year_month) > $employee->norm($request->year_month)) {
-                        $norm_worked = $employee->norm($request->year_month);
+                    if($employee->workhours($year_month) > $employee->norm($year_month)) {
+                        $norm_worked = $employee->norm($year_month);
                     } else {
-                        $norm_worked = $employee->workhours($request->year_month);
+                        $norm_worked = $employee->workhours($year_month);
                     }
-                    if($employee->workhours($request->year_month) > $employee->norm($request->year_month)) {
-                        $overtime = $employee->workhours($request->year_month) - $employee->norm($request->year_month);
+                    if($employee->workhours($year_month) > $employee->norm($year_month)) {
+                        $overtime = $employee->workhours($year_month) - $employee->norm($year_month);
                     } else {
                         $overtime = 0;
                     }
-                    $norm_salary = number_format($norm_worked * $employee->hourly_rate($request->year_month), 2, '.', '');
+                    $norm_salary = number_format($norm_worked * $employee->hourly_rate($year_month), 2, '.', '');
 
                     //overtime_salary integer
-                    $overtime_salary = number_format($overtime * $employee->hourly_rate($request->year_month), 2, '.', '');
+                    $overtime_salary = number_format($overtime * $employee->hourly_rate($year_month), 2, '.', '');
                     //round to integer
                     $overtime_salary = round($overtime_salary);
                     if($employee->getShift()) {
                         $shift_hours = $employee->getShift()->shift_hours();
-                        $norm = $employee->norm($request->year_month);
+                        $norm = $employee->norm($year_month);
                     } else {
                         $shift_hours = '';
                         $norm = 0;
