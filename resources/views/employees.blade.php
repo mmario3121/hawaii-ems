829a5fd0e4cc5b560b8c8438cb66af6a4b69cdb7
Temirlan <temirlan.asanov.ta@gmail.com>
2024-05-31 17:02:39 +0500

    excel-fix

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -85,7 +85,7 @@ use App\Models\Holiday;
                 <td style="color: {{ $workhoursNormDiff >= 0 ? 'green' : 'red' }}" width="5">
                     {{ strval($workhoursNormDiff) }}
                 </td>
-                <td width="30">{{ number_format($employee->company->bin, 0, '', '') }}</td>
+                <td>{{ strval(number_format($employee->company->bin, 0, '', '')) }}</td>
                 <td width="20">{{ $employee->company->title }}</td>
             </tr>
         @endforeach
